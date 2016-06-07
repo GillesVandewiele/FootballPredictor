573887b98ae5cbd394cdf17ad80202f9c24b6a4f
givdwiel <givdwiel.vandewiele@ugent.be>
2016-06-07 15:51:50 +0200

    nice dataset

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -1,6 +1,18 @@
-from lxml import etree
+import urllib
+
+from lxml import etree, html
 
 
-def xml_to_tree(path):
-    return etree.parse(path)
+def get_fifa_rating(player_name, season):
+    url = "http://sofifa.com/players?keyword="+urllib.parse.quote_plus(player_name)+"&v="+season+"&hl=en-US"
+    with urllib.request.urlopen(url) as page:
+        s = page.read().decode("utf-8")
+    tree = html.fromstring(s).getroottree()
+    for table_cell in tree.findall('//td'):
+        if 'data-title' in table_cell.attrib and table_cell.attrib['data-title'] == "Overall rating":
+            rating = table_cell.find('./span').text
+            print((url, int(rating)))
+            return 1, int(rating)
 
+    print((url, 50))
+    return 0, 50
