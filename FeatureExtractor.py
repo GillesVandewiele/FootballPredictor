dceb159ae75b07f9166155c8fb9b6017d5ab64f8
givdwiel <givdwiel.vandewiele@ugent.be>
2016-06-07 19:30:17 +0200

    reading odds now too, and joining the dataframes

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -1,10 +1,33 @@
 import urllib
 
 from lxml import etree, html
+import pandas as pd
+
+
+def get_prior_game_statistics(home_team, away_team, game_date, df, N_GAMES=5):
+
+    print(game_date, 'HOME TEAM: ', home_team, 'AWAY TEAM: ', away_team)
+    # Filtering out some dataframes
+    home_home_games = df[(df.date < game_date) & (df.home_team == home_team)].tail(N_GAMES)
+
+    home_games = df[(df.date < game_date) & ((df.home_team == home_team) | (df.away_team == home_team))].tail(N_GAMES)
+
+    away_away_games = df[(df.date < game_date) & (df.home_team == away_team)].tail(N_GAMES)
+
+    away_games = df[(df.date < game_date) & ((df.home_team == away_team) | (df.away_team == away_team))].tail(N_GAMES)
+
+    homevsaway_homestadium_games = df[(df.date < game_date) & (df.home_team == home_team) &
+                                      (df.away_team == away_team)].tail(N_GAMES)
+
+    homevsaway_games = df[(df.date < game_date) & ((df.home_team == home_team) | (df.away_team == home_team)) &
+                          ((df.away_team == away_team) | (df.home_team == away_team))].tail(N_GAMES)
+
+    print(home_games['date'])
+    pass
 
 
 def get_fifa_rating(player_name, season):
-    url = "http://sofifa.com/players?keyword="+urllib.parse.quote_plus(player_name)+"&v="+season+"&hl=en-US"
+    url = "http://sofifa.com/players?keyword=" + urllib.parse.quote_plus(player_name) + "&v=" + season + "&hl=en-US"
     with urllib.request.urlopen(url) as page:
         s = page.read().decode("utf-8")
     tree = html.fromstring(s).getroottree()
--- XMLExtractor.py
+++ XMLExtractor.py
@@ -3,6 +3,24 @@ from datetime import datetime
 from lxml import etree
 import pandas as pd
 
+import FeatureExtractor
+
+
+def files_to_odds(file_paths):
+    df = None
+    for file_path in file_paths:
+        print('reading...', file_path)
+        betting_odds = pd.read_csv(file_path)
+        if df is None:
+            df = betting_odds
+        else:
+            df = pd.concat([df, betting_odds])
+
+    df['Date'] = df['Date'].apply(lambda x: datetime.strptime(x, '%d/%m/%y'))
+    df['HomeTeam'] = df['HomeTeam'].replace('Dundee', 'Dundee FC')
+    df['AwayTeam'] = df['AwayTeam'].replace('Dundee', 'Dundee FC')
+    df = df.rename(columns={'Date': 'date', 'AwayTeam': 'away_team', 'HomeTeam': 'home_team'})
+    return df[['date', 'away_team', 'home_team', 'B365A', 'B365H', 'B365D']]
 
 def files_to_df(file_paths):
     df = None
@@ -10,7 +28,9 @@ def files_to_df(file_paths):
         print('reading...', file_path)
         df = get_matches_info(xml_to_tree(file_path), df)
 
-    print(df)
+    df['home_team'] = df['home_team'].replace('St.Johnstone', 'St Johnstone')
+    df['away_team'] = df['away_team'].replace('St.Johnstone', 'St Johnstone')
+    return df.sort_values(by='date', ascending=True).reset_index(drop=True)
 
 
 def xml_to_tree(path):
@@ -19,13 +39,14 @@ def xml_to_tree(path):
 
 def get_matches_info(tree, df):
     # TODO: currently not extracted: the sub details and goal details (in which minute, which (who) sub/goal was made)
-    # TODO: subs are useful: in real-life you need to  
+    # TODO: subs are useful: in real-life you need the previous game formation (you don't know it for the game to come)
     # TODO: goals too (e.g: having a goal getter on the field is mostly an advantage)
     new_records = []
     for match in tree.getroot().iter('Match'):
         if match is not None:
             new_record = {}
-            new_record['date'] = datetime.strptime(match.find('Date').text[:-6], '%Y-%m-%dT%H:%M:%S')
+            date = datetime.strptime(match.find('Date').text[:-6], '%Y-%m-%dT%H:%M:%S')
+            new_record['date'] = datetime(year=date.year, month=date.month, day=date.day)
 
             # Home Team
             new_record['home_team'] = match.find('HomeTeam').text
@@ -37,14 +58,19 @@ def get_matches_info(tree, df):
             new_record['home_red'] = match.find('HomeRedCards').text
             new_record['home_formation'] = match.find('HomeTeamFormation').text if match.find(
                 'HomeTeamFormation') is not None else None
-            new_record['home_gk'] = match.find('HomeLineupGoalkeeper').text[1:].split('; ') if match.find(
-                'HomeLineupGoalkeeper') is not None and match.find('HomeLineupGoalkeeper').text is not None else None
-            new_record['home_def'] = match.find('HomeLineupDefense').text[1:].split('; ') if match.find(
-                'HomeLineupDefense') is not None and match.find('HomeLineupDefense').text is not None else None
-            new_record['home_mid'] = match.find('HomeLineupMidfield').text[1:].split('; ') if match.find(
-                'HomeLineupMidfield') is not None and match.find('HomeLineupMidfield').text is not None else None
-            new_record['home_atk'] = match.find('HomeLineupForward').text[1:].split('; ') if match.find(
-                'HomeLineupForward') is not None and match.find('HomeLineupForward').text is not None else None
+            new_record['home_gk'] = [word.lstrip() for word in match.find('HomeLineupGoalkeeper').text.split(';')] \
+                if match.find('HomeLineupGoalkeeper') is not None and match.find('HomeLineupGoalkeeper').text is not \
+                                                                      None else None
+            new_record['home_def'] = [word.lstrip() for word in match.find('HomeLineupDefense').text.split(';')] \
+                if match.find('HomeLineupDefense') is not None and match.find('HomeLineupDefense').text is not \
+                                                                      None else None
+            
+            new_record['home_mid'] = [word.lstrip() for word in match.find('HomeLineupMidfield').text.split(';')] \
+                if match.find('HomeLineupMidfield') is not None and match.find('HomeLineupMidfield').text is not \
+                                                                      None else None
+            new_record['home_atk'] = [word.lstrip() for word in match.find('HomeLineupForward').text.split(';')] \
+                if match.find('HomeLineupForward') is not None and match.find('HomeLineupForward').text is not \
+                                                                      None else None
             new_record['home_FT_goals'] = match.find('HomeGoals').text
             new_record['home_FT_goals'] = match.find('HalfTimeHomeGoals').text
 
@@ -58,14 +84,19 @@ def get_matches_info(tree, df):
             new_record['away_red'] = match.find('AwayRedCards').text
             new_record['away_formation'] = match.find('AwayTeamFormation').text if match.find(
                 'AwayTeamFormation') is not None else None
-            new_record['away_gk'] = match.find('AwayLineupGoalkeeper').text[1:].split('; ') if match.find(
-                'AwayLineupGoalkeeper') is not None and match.find('AwayLineupGoalkeeper').text is not None else None
-            new_record['away_def'] = match.find('AwayLineupDefense').text[1:].split('; ') if match.find(
-                'AwayLineupDefense') is not None and match.find('AwayLineupDefense').text is not None else None
-            new_record['away_mid'] = match.find('AwayLineupMidfield').text[1:].split('; ') if match.find(
-                'AwayLineupMidfield') is not None and match.find('AwayLineupMidfield').text is not None else None
-            new_record['away_atk'] = match.find('AwayLineupForward').text[1:].split('; ') if match.find(
-                'AwayLineupForward') is not None and match.find('AwayLineupForward').text is not None else None
+            new_record['away_gk'] = [word.lstrip() for word in match.find('AwayLineupGoalkeeper').text.split(';')] \
+                if match.find('AwayLineupGoalkeeper') is not None and match.find('AwayLineupGoalkeeper').text is not \
+                                                                      None else None
+            new_record['away_def'] = [word.lstrip() for word in match.find('AwayLineupDefense').text.split(';')] \
+                if match.find('AwayLineupDefense') is not None and match.find('AwayLineupDefense').text is not \
+                                                                      None else None
+            
+            new_record['away_mid'] = [word.lstrip() for word in match.find('AwayLineupMidfield').text.split(';')] \
+                if match.find('AwayLineupMidfield') is not None and match.find('AwayLineupMidfield').text is not \
+                                                                      None else None
+            new_record['away_atk'] = [word.lstrip() for word in match.find('AwayLineupForward').text.split(';')] \
+                if match.find('AwayLineupForward') is not None and match.find('AwayLineupForward').text is not \
+                                                                      None else None
             new_record['away_FT_goals'] = match.find('AwayGoals').text
             new_record['away_FT_goals'] = match.find('HalfTimeAwayGoals').text
 
@@ -77,5 +108,18 @@ def get_matches_info(tree, df):
         return pd.DataFrame.from_records(new_records)
 
 
-files_to_df(['FootballData/scotland1011.xml', 'FootballData/scotland1112.xml', 'FootballData/scotland1213.xml',
-             'FootballData/scotland1314.xml', 'FootballData/scotland1415.xml', 'FootballData/scotland1516.xml'])
+df = files_to_df(['FootballData/scotland1011.xml', 'FootballData/scotland1112.xml', 'FootballData/scotland1213.xml',
+                  'FootballData/scotland1314.xml', 'FootballData/scotland1415.xml', 'FootballData/scotland1516.xml'])
+odds_df = files_to_odds(['FootballData/scotland1112_odds.csv', 'FootballData/scotland1213_odds.csv',
+                         'FootballData/scotland1314_odds.csv', 'FootballData/scotland1415_odds.csv',
+                         'FootballData/scotland1516_odds.csv'])
+
+joined_df = pd.merge(df, odds_df, how='left', on=['date', 'home_team', 'away_team'])
+
+print(joined_df.head(10))
+print(joined_df.tail(10))
+
+
+last_game = df.iloc[len(df) - 1, :]
+print(FeatureExtractor.get_prior_game_statistics(last_game['home_team'], last_game['away_team'],
+                                                 last_game['date'], df))
